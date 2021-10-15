7421a0163d94233198afe6248f47aeb3eecb45d4
SebGoll <50230837+SebGoll@users.noreply.github.com>
2021-10-15 09:36:05 +0200

    Revert "Revert "squelette du projet""
    This reverts commit 87baacfa62ad3ce335a5863fb4c350061398b2a2.

---
+++ PLD-Agile-H4114/src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static String returnString(){
+        return("ça marche");
+    }
+}
