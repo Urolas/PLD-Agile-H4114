566751ea67fca31fea5a1546bc56e7026bb79a0e
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2021-10-16 10:20:38 +0200

    Update Deserializer

--- PLD-Agile-H4114/src/xml/XMLDeserializer.java
+++ PLD-Agile-H4114/src/xml/XMLDeserializer.java
@@ -1,6 +1,25 @@
-package xml;
+package src.xml;
 
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.lang.String;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import model.CityMap;
+import model.Intersection;
+import model.Road;
+import model.Distribution;
+import model.Request;
+import model.DepotAddress;
 
 /**
  * @author 4IF-4114
@@ -22,18 +41,25 @@ public class XMLDeserializer {
      * @param cityMap 
      * @return
      */
-    public static void loadCityMap(CityMap cityMap) {
-        // TODO implement here
-        return null;
+    public static void loadCityMap(CityMap cityMap) throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
+        File xml = XMLfileOpener.getInstance().open(true);
+        DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = docBuilder.parse(xml);
+        Element root = document.getDocumentElement();
+        if (root.getNodeName().equals("plan")) {
+            buildFromDOMXML(root, cityMap);
+        }
+        else {
+            throw new ExceptionXML("Wrong format");
+        }
     }
 
     /**
      * @param distribution 
      * @return
      */
-    public static void loadDistribution(DIstribution distribution) {
+    public static void loadDistribution(Distribution distribution) {
         // TODO implement here
-        return null;
     }
 
     /**
@@ -41,9 +67,24 @@ public class XMLDeserializer {
      * @param cityMap 
      * @return
      */
-    private static void buildCityMapFromDOMXML(void rootDOMNode, CityMap cityMap) {
-        // TODO implement here
-        return null;
+    private static void buildCityMapFromDOMXML(void rootDOMNode, CityMap cityMap) throws ExceptionXML, NumberFormatException {
+        int height = Integer.parseInt(rootDOMNode.getAttribute("height"));
+        if (height <= 0)
+            throw new ExceptionXML("Error when reading file: The plan height must be positive");
+        int width = Integer.parseInt(rootDOMNode.getAttribute("width"));
+        if (width <= 0)
+            throw new ExceptionXML("Error when reading file: The plan width must be positive");
+        cityMap.reset(width,height);
+
+        NodeList intersectionList = rootDOMNode.getElementsByTagName("intersection");
+        for (int i = 0; i < intersectionList.getLength(); i++) {
+            cityMap.add(createIntersection((Element) intersectionList.item(i)));
+        }
+
+        NodeList roadList = rootDOMNode.getElementsByTagName("segment");
+        for (int i = 0; i < roadList.getLength(); i++) {
+            cityMap.add(createRoad((Element) roadList.item(i)));
+        }
     }
 
     /**
@@ -51,9 +92,8 @@ public class XMLDeserializer {
      * @param distribution 
      * @return
      */
-    public static final void buildDistributionFromDOMXML(Element rootDOMNode, Distribution distribution) {
+    public static final void buildDistributionFromDOMXML(Element rootDOMNode, Distribution distribution)  {
         // TODO implement here
-        return null;
     }
 
     /**
@@ -61,8 +101,11 @@ public class XMLDeserializer {
      * @return
      */
     private static Intersection createIntersection(Element elt) {
-        // TODO implement here
-        return null;
+        int id = Integer.parseInt(elt.getAttribute("id"));
+        int latitude = Integer.parseInt(elt.getAttribute("latitude"));
+        int longitude = Integer.parseInt(elt.getAttribute("longitude"));
+
+        return new Intersection(id,latitude,longitude);
     }
 
     /**
@@ -70,8 +113,12 @@ public class XMLDeserializer {
      * @return
      */
     private static Road createRoad(Element elt) {
-        // TODO implement here
-        return null;
+        int id1 = Integer.parseInt(elt.getAttribute("origin"));
+        int id2 = Integer.parseInt(elt.getAttribute("destination"));
+        String name = elt.getAttribute("name");
+        int length = Integer.parseInt(elt.getAttribute("length"));
+
+        return new Road(id1,id2,name,length);
     }
 
     /**
