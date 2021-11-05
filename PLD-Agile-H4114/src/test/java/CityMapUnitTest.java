8c4fdbcf70b363eb176ced14ec4b09951f80211d
SebGoll <50230837+SebGoll@users.noreply.github.com>
2021-11-05 11:41:58 +0100

    Passage des méthodes de désérialisation dans une classe spécifique

--- PLD-Agile-H4114/src/test/java/CityMapUnitTest.java
+++ PLD-Agile-H4114/src/test/java/CityMapUnitTest.java
@@ -19,7 +19,6 @@ import java.util.*;
 
 import static org.junit.Assert.*;
 
-
 public class CityMapUnitTest {
     public static CityMap cm;
     public static HashMap<String,Intersection> intersectionsExpected;
@@ -33,12 +32,13 @@ public class CityMapUnitTest {
         intersectionsExpected = new HashMap<>();
         roadsExpectedSet = new HashMap<>();
         adjacencyListExpected = new HashMap<>();
+
     }
 
     @Before
     public void initialisation(){
         try{
-            loadCityMap(cm,"src/test/testRessources/RouffiacCityMap.xml");
+            XMLDeserializerForTests.loadCityMap(cm,"src/test/testRessources/RouffiacCityMap.xml",intersectionsExpected,roadsExpectedSet,adjacencyListExpected);
         }catch (Exception e){
             System.err.println("Erreur lors de la lecture du fichier");
         }
@@ -132,69 +132,6 @@ public class CityMapUnitTest {
         assertEquals("CityMap{roads="+roadsExpectedSet+"}",cm.toString());
     }
 
-    public static void loadCityMap(CityMap cityMap, String path) throws ParserConfigurationException, SAXException, IOException, XMLException {
-
-        File xml = new File(path);
-        DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        Document document = docBuilder.parse(xml);
-        Element root = document.getDocumentElement();
-        if (root.getNodeName().equals("map")) {
-            buildCityMapFromDOMXML(root, cityMap);
-        } else {
-            throw new XMLException("Wrong format");
-        }
-    }
-
-    private static void buildCityMapFromDOMXML(Element rootDOMNode, CityMap cityMap) throws NumberFormatException {
-
-        cityMap.reset();
-        Double maxLatitude = null;
-        Double minLatitude = null;
-        Double maxLongitude = null;
-        Double minLongitude = null;
-
-        NodeList intersectionList = rootDOMNode.getElementsByTagName("intersection");
-        for (int i = 0; i < intersectionList.getLength(); i++) {
-            Element elt = (Element) intersectionList.item(i);
-            String id = elt.getAttribute("id");
-            Double latitude = Double.parseDouble(elt.getAttribute("latitude"));
-            Double longitude = Double.parseDouble(elt.getAttribute("longitude"));
-            if (maxLatitude == null || maxLatitude < latitude) {
-                maxLatitude = latitude;
-            }
-            if (minLatitude == null || minLatitude > latitude) {
-                minLatitude = latitude;
-            }
-            if (maxLongitude == null || maxLongitude < longitude) {
-                maxLongitude = longitude;
-            }
-            if (minLongitude == null || minLongitude > longitude) {
-                minLongitude = longitude;
-            }
-            cityMap.addIntersection(new Intersection(id, latitude, longitude));
-            intersectionsExpected.put(id,new Intersection(id,latitude,longitude));
-            adjacencyListExpected.put(id, new LinkedList<>());
-            cityMap.initializeAdjacencyList(id);
-        }
-        cityMap.setHeight(maxLatitude-minLatitude);
-        cityMap.setWidth(maxLongitude-minLongitude);
-        cityMap.setNordPoint(maxLatitude);  // La latitude indique un positionnement Nord-Sud
-        cityMap.setWestPoint(minLongitude); // La longitude indique un positionnement Ouest-Est
-        NodeList roadList = rootDOMNode.getElementsByTagName("segment");
-        for (int i = 0; i < roadList.getLength(); i++) {
-            Element elt = (Element) roadList.item(i);
-            String id1 = elt.getAttribute("origin");
-            String id2 = elt.getAttribute("destination");
-            String name = elt.getAttribute("name");
-            Double length = Double.parseDouble(elt.getAttribute("length"));
-            cityMap.addRoad(name,length, id1, id2);
-            Road r = new Road(name,length);
-            r.addRoads(intersectionsExpected.get(id1),intersectionsExpected.get(id2));
-            roadsExpectedSet.put(new AbstractMap.SimpleEntry<>(id1,id2),r);
-            adjacencyListExpected.get(id1).add(new AbstractMap.SimpleEntry<>(id2,length));
-            cityMap.completeAdjacencyList(id1, id2,length);
-        }
-    }
 
 
 }
