58257c895083b8e0353007034840f26040ee5ebb
SebGoll <50230837+SebGoll@users.noreply.github.com>
2021-11-11 19:49:50 +0100

    Add states summary

--- PLD-Agile-H4114/src/controller/AddState2.java
+++ PLD-Agile-H4114/src/controller/AddState2.java
@@ -5,39 +5,38 @@ import model.Intersection;
 import model.PointOfInterest;
 import view.Window;
 
-import java.awt.*;
-
 public class AddState2 implements State {
+
     private Intersection i1;
     private Integer d1;
 
 
-    public void leftClick(Controller c, Window window, CityMap map, ListOfCommands listOfCommands, Intersection i, PointOfInterest poi) {
+    public void leftClick(Controller c, Window window, CityMap map, ListOfCommands listOfCommands,
+                          Intersection i, PointOfInterest poi) {
 
         if (poi != null) {
-
-            c.addState3.entryAction(this.i1,d1, poi,window);
+            c.addState3.entryAction(this.i1, d1, poi, window);
             c.setCurrentState(c.addState3);
-            window.displayMessage("Placez le deliveryPoint : \nrenseignez une durée");
+            window.displayMessage("Choose the delivery duration and\nchoose the delivery point position on the map.");
 
         } else {
-            window.displayMessage("Erreur aucun point : Apres quel point");
+            window.parsingError("No point choosen error : Click on a valid point.");
 
         }
 
 
     }
+
     @Override
-    public void rightClick(Controller c){
+    public void rightClick(Controller c) {
         c.getCitymap().resetSelected();
         c.setCurrentState(c.tourState);
     }
 
-    protected void entryAction(Intersection i,Integer d) {
+    protected void entryAction(Intersection i, Integer d) {
         this.i1 = i;
-        this.d1=d;
+        this.d1 = d;
     }
-
 
 
 }
