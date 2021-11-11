f30f8be6a4e65b7155ce2b498c3dd08080e4e22f
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2021-11-12 00:16:54 +0100

    comment swap command

--- PLD-Agile-H4114/src/controller/SwapCommand.java
+++ PLD-Agile-H4114/src/controller/SwapCommand.java
@@ -1,3 +1,8 @@
+/**
+ * SwapCommand
+ *
+ * @author 4IF-4114
+ */
 package controller;
 
 import model.CityMap;
@@ -6,7 +11,7 @@ import model.PickupAddress;
 import model.PointOfInterest;
 
 /**
- * @author 4IF-4114
+ * The Command adding swaping the position of two points nearby
  */
 public class SwapCommand implements Command {
 
@@ -17,19 +22,24 @@ public class SwapCommand implements Command {
     private final boolean AUTHORIZED;
 
     /**
-     * Default constructor
+     * Constructor of SwapCommand
+     *
+     * @param map the citymap
+     * @param idPOI id of the point to move
+     * @param direction direction that the point needs to move to
+     * @throws Exception if the movement is impossible
      */
-    public SwapCommand(CityMap map, Integer idPOI, int sens) throws Exception {
+    public SwapCommand(CityMap map, Integer idPOI, int direction) throws Exception {
         this.MAP = map;
         this.POI = map.distribution.getPointOfIntersection(idPOI);
-        this.SENS = sens;
+        this.SENS = direction;
         this.POSITION = map.tour.getPointOfInterests().indexOf(POI);
 
-        if (POSITION + sens == 0
-                || POSITION + sens == map.tour.getPointOfInterests().size() - 1
-                || (POI instanceof PickupAddress && map.tour.getPointOfInterests().get(POSITION + sens)
+        if (POSITION + direction == 0
+                || POSITION + direction == map.tour.getPointOfInterests().size() - 1
+                || (POI instanceof PickupAddress && map.tour.getPointOfInterests().get(POSITION + direction)
                     == map.distribution.getDelivery((PickupAddress) POI))
-                || (POI instanceof DeliveryAddress && map.tour.getPointOfInterests().get(POSITION + sens)
+                || (POI instanceof DeliveryAddress && map.tour.getPointOfInterests().get(POSITION + direction)
                     == map.distribution.getPickup((DeliveryAddress) POI))) {
             this.AUTHORIZED = false;
             throw new Exception("Error : this action is impossible.");
@@ -40,7 +50,7 @@ public class SwapCommand implements Command {
 
 
     /**
-     *
+     *do the command
      */
     public void doCommand() {
         if (AUTHORIZED)
@@ -48,7 +58,7 @@ public class SwapCommand implements Command {
     }
 
     /**
-     *
+     *undo the command
      */
     public void undoCommand() {
         if (AUTHORIZED)
