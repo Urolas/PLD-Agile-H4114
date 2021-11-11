464427cc351782b9e43cb660e4d92220122a8b92
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2021-11-11 21:19:32 +0100

    comment graphoftopoint

--- PLD-Agile-H4114/src/model/GraphPointToPoint.java
+++ PLD-Agile-H4114/src/model/GraphPointToPoint.java
@@ -1,13 +1,25 @@
+/**
+ * GraphPointToPoint
+ * @author 4IF-4114
+ */
 package model;
 
 import java.util.*;
 
+/**
+ * The graph representing the map, having the each point as node and each road as branch
+ */
 public class GraphPointToPoint {
     private int nbVertices;
     private HashMap<Integer, HashMap<Integer, AbstractMap.SimpleEntry<Double,List<String>>>> cost;
     private HashMap<Integer,Integer> constraints;
 
 
+    /**
+     * Constructor of GraphPointToPoint
+     * @param resultsDijkstra the result returned from the Dijstra function
+     * @param constraints the constraints of the priority order of the points of interest
+     */
     public GraphPointToPoint(HashMap<PointOfInterest, HashMap<PointOfInterest, AbstractMap.SimpleEntry<Double,List<String>>>> resultsDijkstra, HashMap<Integer,Integer> constraints) {
         this.nbVertices=resultsDijkstra.size();
         this.constraints=constraints;
@@ -19,15 +31,18 @@ public class GraphPointToPoint {
             }
             cost.put(entry.getKey().idPointOfInterest,currentEntry);
         }
-
     }
 
     public int getNbVertices() {
         return nbVertices;
     }
 
+    /**
+     * get the cost between two nodes of the graph
+     * @param i1 the id of the first node
+     * @param i2 the id of the second node
+     */
     public Double getCost(Integer i1, Integer i2) {
-
         return this.cost.get(i1).get(i2).getKey();
     }
 
