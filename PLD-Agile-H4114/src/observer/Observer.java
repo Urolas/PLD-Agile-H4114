a01473e2dcc4e39ba518bc4595e090b0c0e9f984
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2021-11-11 19:10:05 +0100

    Comment Observable and Observer

--- PLD-Agile-H4114/src/observer/Observer.java
+++ PLD-Agile-H4114/src/observer/Observer.java
@@ -1,16 +1,18 @@
+/**
+ * Observer
+ * @author 4IF-4114
+ */
 package observer;
 
-
-
 /**
- * 
+ * An Observer sub-class to notify the observable when an object is modified
  */
 public interface Observer {
 
     /**
-     * @param observed 
-     * @param object 
-     * @return
+     * Update other objects or views related to the modified object
+     * @param observed the Observable
+     * @param object the modified object
      */
     public void update(Observable observed, Object object);
 
